a54e266762971f9f71bd9f3dc593ec69f2d75133
Pavel Rudenko <santariver@yandex.ru>
2023-06-29 18:56:05 +0500

    init

---
+++ app/Http/Controllers/IntervalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreIntervalRequest;
+use App\Http\Requests\UpdateIntervalRequest;
+use App\Models\Interval;
+
+class IntervalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIntervalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Interval $intervals)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Interval $intervals)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIntervalRequest $request, Interval $intervals)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Interval $intervals)
+    {
+        //
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import "jquery/dist/jquery.js";
+import 'jquery.easing/jquery.easing.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'chart.js/dist/Chart.min.js'
+
+import './sb-admin-2.js';
+import './demo/chart-area-demo.js'
+import './demo/chart-bar-demo.js'
+import './demo/chart-pie-demo.js'
+import './demo/datatables-demo.js'
